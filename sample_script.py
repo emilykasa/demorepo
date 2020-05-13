b9e9f82d9aff912ec924201811898c0b5b759e67
Emily Kasa <ekasa@berkeley.edu>
2020-05-13 10:44:35 -0700

    changing matrix

--- sample_script.py
+++ sample_script.py
@@ -4,9 +4,9 @@ import pandas as pd
 my_matrix = np.eye(4)
 
 my_second_matrix=np.array[[1, 2, 3, 4]
-[5, 6, 7, 8],
-[9, 10, 11, 12],
-[13, 14, 15, 16]]
+[5, 6, 7],
+[9, 10, 11],
+[13, 14, 15]]
 
 print(my_matrix)
 print(my_second_matrix)
