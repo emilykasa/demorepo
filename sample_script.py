63a4ab8af2f02420e79cb269f33a51360fca7108
Emily Kasa <ekasa@berkeley.edu>
2020-05-13 10:41:31 -0700

    created some matrices

---
+++ sample_script.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+my_matrix = np.eye(4)
+
+my_second_matrix=np.array[[1, 2, 3, 4]
+[5, 6, 7, 8],
+[9, 10, 11, 12],
+[13, 14, 15, 16]]
+
+print(my_matrix)
+print(my_second_matrix)
+print("hello")
+print("goodbye")
